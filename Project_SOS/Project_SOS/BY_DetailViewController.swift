e0c808954eb4e301a8fe3a6cd55a3f0c3dcc8590
fimuxd@gmail.com <fimuxd@gmail.com>
2017-09-09 13:34:07 +0900

    [수정] 디테일뷰 구성 수정
    1. 스토리보드 수정
    2. 디테일뷰 구성을, 기존의 테이블뷰 + 테이블뷰 + 콜렉션뷰에서
        뷰 + 스크롤뷰 (스택뷰, 콜렉션뷰) + 뷰 로 수정함

--- Project_SOS/Project_SOS/BY_DetailViewController.swift
+++ Project_SOS/Project_SOS/BY_DetailViewController.swift
@@ -14,18 +14,24 @@ class BY_DetailViewController: UIViewController {
     //MARK:-        Properties                 //
     /*******************************************/
     
-    @IBOutlet weak var summaryTableView: UITableView!
-    @IBOutlet weak var contentsTableView: UITableView!
-    @IBOutlet weak var goToAdditionalInfoCollectionView: UICollectionView!
+    @IBOutlet weak var summaryView: UIView!
+    @IBOutlet weak var mainScrollView: UIScrollView!
+    @IBOutlet weak var characterSelectSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
     
-    @IBOutlet weak var selectCharacterSegmentControl: UISegmentedControl!
+    let maxHeaderHeight:CGFloat = 128
+    let minHeaderHeight:CGFloat = 44
+    
+    var previousScrollOffset:CGFloat = 0
+    
+    @IBOutlet weak var titleTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tagStackView: UIStackView!
     
     /*******************************************/
     //MARK:-         Functions                 //
     /*******************************************/
     
-    @IBAction func selectCharacterSegmentControlAction(_ sender: UISegmentedControl) {
-    }
     
     
     
@@ -36,12 +42,12 @@ class BY_DetailViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         
-        self.summaryTableView.register(UINib.init(nibName: "BY_DetailSummaryTableViewCell", bundle: nil), forCellReuseIdentifier: "DetailSummaryTableViewCell")
-        self.contentsTableView.register(UINib.init(nibName: "BY_DetailTextTableViewCell", bundle: nil), forCellReuseIdentifier: "DetailTextTableViewCell")
-        self.contentsTableView.register(UINib.init(nibName: "BY_DetailImageTableViewCell", bundle: nil), forCellReuseIdentifier: "DetailImageTableViewCell")
-        self.goToAdditionalInfoCollectionView.register(UINib.init(nibName: "BY_DetailCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "DetailCollectionViewCell")
+        self.headerHeightConstraint.constant = self.maxHeaderHeight
+        //updateHeader()
         
+        self.collectionView.register(UINib.init(nibName: "BY_DetailCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "DetailCollectionViewCell")
         awakeFromNib()
+        
     }
     
     override func viewDidLoad() {
@@ -55,6 +61,7 @@ class BY_DetailViewController: UIViewController {
         
         print(selectedCharacter)
         
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -63,52 +70,8 @@ class BY_DetailViewController: UIViewController {
     
 }
 
-//테이블뷰 설정
-extension BY_DetailViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let contentView:UIImageView = UIImageView(image: #imageLiteral(resourceName: "default_HeaderImage"))
-        
-        let headerView: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
-        headerView.addSubview(contentView)
-        return headerView
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if tableView.restorationIdentifier == "SummaryTableView" {
-            let cell:BY_DetailSummaryTableViewCell = tableView.dequeueReusableCell(withIdentifier: "DetailSummaryTableViewCell", for: indexPath) as! BY_DetailSummaryTableViewCell
-            
-            return cell
-            
-        }else{
-            let textCell:BY_DetailTextTableViewCell = tableView.dequeueReusableCell(withIdentifier: "DetailTextTableViewCell", for: indexPath) as! BY_DetailTextTableViewCell
-            let imageCell:BY_DetailImageTableViewCell = tableView.dequeueReusableCell(withIdentifier: "DetailImageTableViewCell", for: indexPath) as! BY_DetailImageTableViewCell
-            
-            return textCell
-            
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if tableView.restorationIdentifier == "SummaryTableView" {
-            return 105
-        }else {
-            return 150
-        }
-    }
-    
-}
-
 //콜렉션뷰 설정
-extension BY_DetailViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+extension BY_DetailViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
     }
@@ -119,19 +82,27 @@ extension BY_DetailViewController: UICollectionViewDataSource, UICollectionViewD
         switch indexPath.item {
         case 0:
             cell.imageView.image = #imageLiteral(resourceName: "default_Profile")
-            cell.textLabel.text = "고구마에게 메일링"
+            cell.textLabel.text = "고구마에게\n메일링"
         case 1:
             cell.imageView.image = #imageLiteral(resourceName: "default_Google")
-            cell.textLabel.text = "이 키워드로 구글링"
+            cell.textLabel.text = "이 키워드로\n구글링"
         case 2:
-            cell.imageView.image = #imageLiteral(resourceName: "defailt_Naver")
-            cell.textLabel.text = "이 키워드로 네이빙"
+            cell.imageView.image = #imageLiteral(resourceName: "default_Naver")
+            cell.textLabel.text = "이 키워드로\n네이빙"
         default: cell.imageView.image = #imageLiteral(resourceName: "default_Profile")
         }
         
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.init(width: 80, height: 80*7/5)
+    }
+    
+    
+
+    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // 메일링하기
         // 구글링하기
