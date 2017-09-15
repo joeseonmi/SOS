e25819027a100077a10212e1451fd0d001f997d6
leejaesung <blackturtle2@gmail.com>
2017-09-16 00:13:09 +0900

    [추가] 애드몹 테스트를 위한 뷰컨트롤러와 스토리보드 임시 추가
    원활한 테스트를 위해 `JS_AdMobTest`라는 뷰컨트롤러와 스토리보드를 추가하였습니다.
    나중에 지울 소스이고, 깔끔하게 날릴 수 있도록, 실제 폴더도 구분지어 두었습니다.

---
+++ Project_SOS/Project_SOS/JS_AdmobTest/JS_AdMobTestViewController.swift
@@ -0,0 +1,24 @@
+//
+//  JSAdMobTestViewController.swift
+//  Project_SOS
+//
+//  Created by leejaesung on 2017. 9. 16..
+//  Copyright © 2017년 joe. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class JS_AdMobTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
