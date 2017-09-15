90ffb4c97017ba5cbeed4990e3c78d4c45224994
leejaesung <blackturtle2@gmail.com>
2017-09-16 00:26:00 +0900

    [추가] 애드몹 테스트 코드
    애드몹을 띄우는 것까지.. 지금은 화면 최상단에 뜬금없이 구글 광고가 보입니다.

--- Project_SOS/Project_SOS/JS_AdmobTest/JS_AdMobTestViewController.swift
+++ Project_SOS/Project_SOS/JS_AdmobTest/JS_AdMobTestViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 import GoogleMobileAds
 
 class JS_AdMobTestViewController: UIViewController {
+    
+    var bannerView: GADBannerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        bannerView = GADBannerView(adSize: kGADAdSizeFullBanner)
+        self.view.addSubview(bannerView)
+        
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        
+        bannerView.load(GADRequest())
     }
 
     override func didReceiveMemoryWarning() {
