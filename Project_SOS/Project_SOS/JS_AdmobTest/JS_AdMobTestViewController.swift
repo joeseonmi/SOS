485089f35154165471644e3e88e4c0ca60f61729
leejaesung <blackturtle2@gmail.com>
2017-09-20 16:10:38 +0900

    [추가] 구글 애드몹 코드 구현 완료
    구글 애드몹 관련 코드를 모두 구현하였습니다.
    아마 선미님이 만드시는 디테일 뷰에 소스를 추가하면 되고, 주석으로 설명은 모두 달아두었으니, 추가하는 건 `참 쉽죠?` 입니다. 훗-

--- Project_SOS/Project_SOS/JS_AdmobTest/JS_AdMobTestViewController.swift
+++ Project_SOS/Project_SOS/JS_AdmobTest/JS_AdMobTestViewController.swift
@@ -11,24 +11,41 @@ import GoogleMobileAds
 
 class JS_AdMobTestViewController: UIViewController {
     
+    @IBOutlet var uiViewAdmobBannerBackgroundView:UIView!
+    // 스토리보드에서 UIView를 화면 최하단에 삽입하고, 오토레이아웃의 높이는 50으로 설정합니다.
+    
     var bannerView: GADBannerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bannerView = GADBannerView(adSize: kGADAdSizeFullBanner)
-        self.view.addSubview(bannerView)
+        self.addAdMobView()
         
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-        bannerView.rootViewController = self
-        
-        bannerView.load(GADRequest())
- 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    
+    /*******************************************/
+    //MARK:-         Functions                 //
+    /*******************************************/
+    
+    // MARK: AdMob을 UIView에 추가하는 메소드입니다.
+    // IBOutlet에서 UIView를 추가하고, 작업하면 됩니다.
+    func addAdMobView() {
+        bannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+        // adSize에는 6가지 종류가 있고, 가장 보편적인 사이즈는 '320*50'입니다.
+        // 쏘쓰에는 아이폰의 종류에 따라 광고 사이즈가 변하는 SmartBanner를 선택했습니다.
+        // 참고: https://developers.google.com/admob/ios/banner?hl=ko
+        
+//        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716" // 테스트 adUnitID
+        bannerView.adUnitID = "ca-app-pub-9821073709980211/5330955915" // 실제 사용하는 adUnitID
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        
+        self.uiViewAdmobBannerBackgroundView.addSubview(bannerView)
+    }
 
 }
