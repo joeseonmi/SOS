352e76af6fc4d2bdd397afe726317358eb1b95a2
fimuxd@gmail.com <fimuxd@gmail.com>
2017-09-23 19:07:26 +0900

    git

---
+++ Project_SOS/Project_SOS/BY_DetailTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  BY_DetailTableViewCell.swift
+//  Project_SOS
+//
+//  Created by Bo-Young PARK on 22/9/2017.
+//  Copyright © 2017 joe. All rights reserved.
+//
+
+import UIKit
+
+class BY_DetailTableViewCell: UITableViewCell {
+
+    /*******************************************/
+    //MARK:-        Properties                 //
+    /*******************************************/
+    
+    @IBOutlet weak var characterIconImage: UIImageView!
+    @IBOutlet weak var explainBubbleText: UILabel!
+    @IBOutlet weak var explainBubbleImage: UIImageView!
+    
+    /*******************************************/
+    //MARK:-        LifeCycle                  //
+    /*******************************************/
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    /*******************************************/
+    //MARK:-         Functions                 //
+    /*******************************************/
+    
+}
