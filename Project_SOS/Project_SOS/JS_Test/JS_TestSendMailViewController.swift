bd05eab6ef47e38196070324600e53edc6b25b23
leejaesung <blackturtle2@gmail.com>
2017-09-28 01:31:18 +0900

    [update] JS_ADmobTest 등 폴더명 변경 및 기타 클래스명 변경
    처음에 테스트를 위한 폴더명을 `ADmobTest`라고 지었는데, 계속 작업 / 테스트하는 폴더로 사용하고 있어서, 일관성 있게 파일명과 클래스명들을 바꾸었습니다.
    크게 신경 쓰지 않으셔도 됩니다.

---
+++ Project_SOS/Project_SOS/JS_Test/JS_TestSendMailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  JS_SendMailTestViewController.swift
+//  Project_SOS
+//
+//  Created by leejaesung on 2017. 9. 28..
+//  Copyright © 2017년 joe. All rights reserved.
+//
+
+import UIKit
+
+class JS_TestSendMailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
