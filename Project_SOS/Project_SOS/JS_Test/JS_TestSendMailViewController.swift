68e97fb3da4e221250e8d752d245d5f5754108ae
leejaesung <blackturtle2@gmail.com>
2017-09-28 02:18:44 +0900

    [Create] 메일 보내기 기능 구현 완료
    메일 보내기 기능을 구현 완료하였습니다.
    정상적으로 메일이 보내지는지 성공 테스트까지 완료하였습니다. :ok:

--- Project_SOS/Project_SOS/JS_Test/JS_TestSendMailViewController.swift
+++ Project_SOS/Project_SOS/JS_Test/JS_TestSendMailViewController.swift
@@ -7,17 +7,61 @@
 //
 
 import UIKit
+import MessageUI
 
 class JS_TestSendMailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    // 메일 보내기 버튼 IBAction 정의
+    @IBAction func buttonSendMailAction(sender: UIButton) {
+        let JS_email = "blackturtle2@gmail.com"
+        
+        // email 주소를 parameter로 담아서 function에 태우면 됩니다.
+        // 우리 SOS에서는 사용자 캐릭터 별로 메일 주소를 다르게 해야 합니다.
+        sendEmailTo(emailAddress: JS_email)
+    }
+    
+    // MARK: 메일 보내기 function 정의
+    // [주의] `MessageUI` import가 필요합니다!
+    func sendEmailTo(emailAddress email:String) {
+        let userSystemVersion = UIDevice.current.systemVersion // 현재 사용자 iOS 버전
+        let userAppVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String // 현재 사용자 SOS 앱 버전
+        
+        // 메일 쓰는 뷰컨트롤러 선언
+        let mailComposeViewController = configuredMailComposeViewController(emailAddress: email, systemVersion: userSystemVersion, appVersion: userAppVersion!)
+        
+        //사용자의 아이폰에 메일 주소가 세팅되어 있을 경우에만 mailComposeViewController()를 태웁니다.
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } // else일 경우, iOS 에서 자체적으로 메일 주소를 세팅하라는 메시지를 띄웁니다.
+    }
+    
+    // MARK: 메일 보내는 뷰컨트롤러 속성 세팅
+    func configuredMailComposeViewController(emailAddress:String, systemVersion:String, appVersion:String) -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // 메일 보내기 Finish 이후의 액션 정의를 위한 Delegate 초기화.
+        
+        mailComposerVC.setToRecipients([emailAddress]) // 받는 사람 설정
+        mailComposerVC.setSubject("[SOS] 사용자로부터 도착한 편지") // 메일 제목 설정
+        mailComposerVC.setMessageBody("* iOS Version: \(systemVersion) / App Version: \(appVersion)\n** 고맙습니다. 무엇이 궁금하신가요? :D", isHTML: false) // 메일 내용 설정
+        
+        return mailComposerVC
+    }
+    
+}
 
+// MARK: Extension - MFMailComposeViewControllerDelegate
+extension JS_TestSendMailViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
 }
